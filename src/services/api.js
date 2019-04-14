d44301505ca1d359d2c812394025241a5babbf0c
Ygor Azambuja <ygorazambuja@gmail.com>
2019-04-14 17:56:44 -0400

    aplicando cors no headers da Api.

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://bolota-share-torrent.herokuapp.com'
+  baseURL: 'https://bolota-share-torrent.herokuapp.com',
+  headers: {
+    crossdomain: true
+  }
 });
 
 export default api;
